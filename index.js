371387c38cc79cb97d2d2f2234f2ed9e7c909c38
Michael Myung <michael.h.myung@gmail.com>
2018-02-14 21:27:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+  let playList = []
+  for (let i; i < musicians.length; i++) {
+    if (i === 0) {
+      playlist.push(`${musicians[0]} plays ${instruments[0]}`);
+    }
+  }
+}
