41393597b5c22b21737a8e7f29b63cc18371af67
Michael Myung <michael.h.myung@gmail.com>
2018-02-14 21:28:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
   let playList = []
-  
+
   for (let i; i < musicians.length; i++) {
     playlist.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+
+  return playList
 }
